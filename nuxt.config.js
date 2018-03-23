f7b0dff2bf49e692595091b6e8bb282cfa833e84
Andrej Stieben <e-mail@stieben.de>
2018-03-23 20:13:46 +0100

    feat: add favicon

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  head: {
+    link: [
+      {
+        color: '#36d1c4',
+        href: '/safari-pinned-tab.svg?v=1',
+        rel: 'mask-icon'
+      },
+      {
+        href: '/apple-touch-icon.png?v=1',
+        rel: 'apple-touch-icon',
+        sizes: '180x180'
+      },
+      {
+        href: '/favicon-16x16.png?v=1',
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        href: '/favicon-32x32.png?v=1',
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        href: '/favicon.ico?v=1',
+        rel: 'shortcut icon'
+      },
+      {
+        href: '/site.webmanifest?v=1',
+        rel: 'manifest'
+      }
+    ],
+    meta: [
+      {
+        content: '#36d1c4',
+        name: 'msapplication-TileColor'
+      },
+      {
+        content: '#36d1c4',
+        name: 'theme-color'
+      }
+    ]
+  }
+};
