d90ea0ef95018026b21a0a1d4eabc09f620785bf
Andrej Stieben <e-mail@stieben.de>
2021-04-13 16:55:28 +0300

    feat: add error pages

--- layouts/error.js
+++ layouts/error.js
@@ -1,11 +1,26 @@
 export default {
-  head () {
+  created () {
+    this.initialiseMessage();
+  },
+  data () {
     return {
-      bodyAttrs: {
-        class: 'error-page'
-      },
-      title: this.error.message + ' · peerhaven'
+      messageBody: 'An error occurred. Sorry!',
+      messageTitle: 'Error'
     };
+  },
+  head: {
+    bodyAttrs: {
+      class: 'error-page'
+    },
+    title: this.error.message + ' · peerhaven'
+  },
+  methods: {
+    initialiseMessage: function () {
+      if (this.error.statusCode === 404) {
+        this.messageBody = 'The page you were looking for could not be found. If that causes you inconvenience, please get in touch with us.';
+        this.messageTitle = 'Page not found';
+      }
+    }
   },
   props: ['error']
 };
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,5 @@
 <script src="./error.js"></script>
 
-<style lang="scss">
-article.message .media {
-  color: $turquoise;
-}
-</style>
-
 <template>
   <div class="error-container">
     <img
@@ -13,24 +7,11 @@ article.message .media {
       src="~/assets/img/aghast-emoticon.png"
     />
     <h1>Oh snap!</h1>
-    <article class="message is-warning">
-      <div class="message-header">
-        <h2 v-if="error.statusCode === 404">Page not found</h2>
-        <h2 v-else>An error occurred</h2>
-      </div>
-      <div class="message-body">
-        <p>
-          At some point, someone has somehow made a mistake.
-          If you believe it was us, please get in touch.
-        </p>
-        <p>
-          Otherwise, may we offer you a
-          <nuxt-link class="button" to="/">link back to our front page</nuxt-link>?
-        </p>
-      </div>
-    </article>
-    <b-message title="Warning" type="is-warning" v-bind:closable="false">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit
+    <b-message :closable="false" :title="messageTitle" type="is-warning">
+      <p>{{ messageBody }}</p>
+      <p>
+        <nuxt-link class="button" to="/">Back to front page</nuxt-link>
+      </p>
     </b-message>
   </div>
 </template>
