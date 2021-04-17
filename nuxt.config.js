1a45641dc4819e01056d179a7d8b0da60646f125
Andrej Stieben <e-mail@stieben.de>
2021-04-17 14:17:39 +0300

    style: update Nuxt config & JS formatting

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,47 +15,49 @@ export default {
   },
 
   buildModules: ['@nuxtjs/style-resources'],
+  components: true, // auto import components
   css: ['assets/scss/main'],
 
   head: {
-    link: [{
-      color: '#36d1c4',
-      href: '/safari-pinned-tab.svg?v=1',
-      rel: 'mask-icon'
-    },
-    {
-      href: '//fonts.googleapis.com/css?family=Comfortaa:300&amp;subset=cyrillic,cyrillic-ext,greek,latin-ext',
-      rel: 'stylesheet'
-    },
-    {
-      href: '//fonts.googleapis.com/css?family=Maven+Pro:400,500&amp;subset=latin-ext',
-      rel: 'stylesheet'
-    },
-    {
-      href: '/apple-touch-icon.png?v=1',
-      rel: 'apple-touch-icon',
-      sizes: '180x180'
-    },
-    {
-      href: '/favicon-16x16.png?v=1',
-      rel: 'icon',
-      sizes: '16x16',
-      type: 'image/png'
-    },
-    {
-      href: '/favicon-32x32.png?v=1',
-      rel: 'icon',
-      sizes: '32x32',
-      type: 'image/png'
-    },
-    {
-      href: '/favicon.ico?v=1',
-      rel: 'shortcut icon'
-    },
-    {
-      href: '/site.webmanifest?v=1',
-      rel: 'manifest'
-    }
+    link: [
+      {
+        color: '#36d1c4',
+        href: '/safari-pinned-tab.svg?v=1',
+        rel: 'mask-icon'
+      },
+      {
+        href: '//fonts.googleapis.com/css?family=Comfortaa:300&amp;subset=cyrillic,cyrillic-ext,greek,latin-ext',
+        rel: 'stylesheet'
+      },
+      {
+        href: '//fonts.googleapis.com/css?family=Maven+Pro:400,500&amp;subset=latin-ext',
+        rel: 'stylesheet'
+      },
+      {
+        href: '/apple-touch-icon.png?v=1',
+        rel: 'apple-touch-icon',
+        sizes: '180x180'
+      },
+      {
+        href: '/favicon-16x16.png?v=1',
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        href: '/favicon-32x32.png?v=1',
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        href: '/favicon.ico?v=1',
+        rel: 'shortcut icon'
+      },
+      {
+        href: '/site.webmanifest?v=1',
+        rel: 'manifest'
+      }
     ],
     meta: [ // charset and viewport should come first
       {
@@ -70,16 +72,16 @@ export default {
         content: 'ie=edge'
       },
       {
-        content: '/browserconfig.xml',
-        name: 'msapplication-config'
-      },
-      {
         content: '#36d1c4',
         name: 'msapplication-TileColor'
       },
       {
         content: '#36d1c4',
         name: 'theme-color'
+      },
+      {
+        content: '/browserconfig.xml',
+        name: 'msapplication-config'
       },
       {
         content: 'index,follow',
@@ -111,26 +113,32 @@ export default {
 
   loading: { // custom progress bar
     color: '#f6318c',
+    continuous: true,
     failedColor: '#ff3860'
   },
 
   modules: [
-    ['nuxt-buefy', {
-      css: false // use SCSS imports instead
-    }],
-    ['nuxt-matomo', {
-      cookies: true,
-      matomoUrl: process.env.MATOMO_URL,
-      siteId: process.env.MATOMO_SITE_ID
-    }]
+    [
+      'nuxt-buefy',
+      {
+        css: false // use SCSS imports instead
+      }
+    ],
+    [
+      'nuxt-matomo',
+      {
+        cookies: true,
+        matomoUrl: process.env.MATOMO_URL,
+        siteId: process.env.MATOMO_SITE_ID
+      }
+    ]
   ],
 
   render: {
     csp: true // see https://github.com/nuxt/nuxt.js/pull/2549
   },
+
   styleResources: {
-    scss: [
-      './assets/scss/implicit.scss'
-    ]
+    scss: ['./assets/scss/implicit.scss']
   }
 };
