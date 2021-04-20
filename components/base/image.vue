peerhaven/web

commit 49ba02bce6a9c3424387dde3a0ef46cae139e26d

feat: add BaseImage component to deal with varying pixel ratios
Pictures are now provided in 4 resolutions and 2 file formats (webp and fallback).

> close #14

components/base/image.vue
@@ -1,0 +1,78 @@
+<script>
+export default {
+  data () {
+    return {
+      pixelRatioList: [
+        '1.0x',
+        '1.5x',
+        '2.0x',
+        '3.0x',
+      ]
+    };
+  },
+
+  methods: {
+    getSource: function (extension) {
+      return getImagePath(
+        this.pixelRatioList[0],
+        this.filename,
+        extension
+      );
+    },
+
+    getSourceSet: function (extension) {
+      let value = this.getSource(extension);
+
+      // intentionally starting at 1 because first pixel ratio is already in src
+      for (let i = 1; i < this.pixelRatioList.length; i++) {
+        let imagePath = getImagePath(
+          this.pixelRatioList[i],
+          this.filename,
+          extension
+        );
+        value += `, ${imagePath} ${this.pixelRatioList[i]}`;
+      }
+
+      return value;
+    }
+  },
+
+  props: {
+    fallback: {
+      required: true,
+      type: String
+    },
+    filename: {
+      required: true,
+      type: String
+    },
+    info: String
+  }
+};
+
+/**
+ * Example output: /_nuxt/assets/img/png/1.0x/happy-emoji.png
+ */
+function getImagePath(pixelRatio, filename, extension) {
+  // request the image via webpack by using `require`
+  // https://blog.lichter.io/posts/dynamic-images-vue-nuxt/
+  return require(
+    `~/assets/img/${extension}/${pixelRatio}/${filename}.${extension}`
+  );
+}
+</script>
+
+
+
+
+
+<template>
+  <picture>
+    <source :srcset="getSourceSet('webp')" />
+    <img
+      :alt="info"
+      :srcset="getSourceSet(fallback)"
+      :src="getSource(fallback)"
+    />
+  </picture>
+</template>

layouts/error.vue
@@ -63,9 +63,10 @@ export default {
 
 <template>
   <div>
-    <img
-      alt="aghast emoticon"
-      src="~/assets/img/aghast-emoticon.png"
+    <base-image
+      fallback="png"
+      filename="aghast-emoticon"
+      info="aghast emoticon"
     />
     <h1>Oh snap!</h1>
 

commit 998f8b249761ffeb05e63d9940386f13d4b424cc

fix: several typos, one of them critical
Wrong image file name led to server error (500) on error page (404); bug introduced in commit
49ba02bc.

components/base/image.vue
@@ -23,7 +23,7 @@ export default {
     getSourceSet: function (extension) {
       let value = this.getSource(extension);
 
-      // intentionally starting at 1 because first pixel ratio is already in src
+      // intentionally starting at 1 because first pixel ratio already in value
       for (let i = 1; i < this.pixelRatioList.length; i++) {
         let imagePath = getImagePath(
           this.pixelRatioList[i],
@@ -51,14 +51,16 @@ export default {
 };
 
 /**
- * Example output: /_nuxt/assets/img/png/1.0x/happy-emoji.png
+ * Example output:
+ * - in development: /_nuxt/assets/img/png/1.0x/happy-emoji.png
+ * - in production: /_nuxt/img/happy-emoji.d0882ca.png
  */
 function getImagePath(pixelRatio, filename, extension) {
+  let relativePath = `${extension}/${pixelRatio}/${filename}.${extension}`;
+
   // request the image via webpack by using `require`
   // https://blog.lichter.io/posts/dynamic-images-vue-nuxt/
-  return require(
-    `~/assets/img/${extension}/${pixelRatio}/${filename}.${extension}`
-  );
+  return require('~/assets/img/' + relativePath);
 }
 </script>
 

layouts/error.vue
@@ -65,8 +65,8 @@ export default {
   <div>
     <base-image
       fallback="png"
-      filename="aghast-emoticon"
-      info="aghast emoticon"
+      filename="aghast-emoji"
+      info="aghast emoji"
     />
     <h1>Oh snap!</h1>
 