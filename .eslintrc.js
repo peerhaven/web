7b0dc954f83e09b173e9f4c9f1b8ce091faff38c
Andrej Stieben <e-mail@stieben.de>
2021-04-26 22:31:43 +0300

    feat(security): add console warning to help prevent social engineering

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,11 +29,37 @@ module.exports = {
     // allow require statements inside blocks (only if necessary)
     'global-require': 'off',
 
+    // no file extensions in import statements
+    'import/extensions': [
+      'error',
+      'never'
+    ],
+
     // it seems we need this for asset loading
     'import/no-dynamic-require': 'off',
 
+    // allow aliases
+    'import/no-unresolved': 'off',
+
+    // allow intentional console output
+    'no-console': [
+      'error',
+      {
+        allow: [
+          'error',
+          'warn'
+        ]
+      }
+    ],
+
     // allow in single-file components (according to HTML style guide)
     'no-multiple-empty-lines': 'off',
+
+    // operators in multiline expressions at the beginning of the line
+    'operator-linebreak': [
+      'error',
+      'before'
+    ],
 
     // use alphabetic sorting (script > style > template)
     'vue/component-tags-order': 'off',
