520996a6d95880785894e91946897c9f8303cfa3
Andrej Stieben <e-mail@stieben.de>
2021-04-28 00:48:11 +0300

    build: update ESLint config and Vue style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-
   extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'semistandard'
+    './node_modules/@peerhaven/peerhaven/.eslintrc.js',
+    'plugin:vue/recommended'
   ],
 
-  ignorePatterns: [
-    '.git',
-    '.nuxt',
-    'node_modules'
-  ],
-
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-
+  ignorePatterns: ['.nuxt'],
   plugins: ['vue'],
+  root: true,
 
   rules: {
-    // allow require statements inside blocks (only if necessary)
-    'global-require': 'off',
-
-    // no file extensions in import statements
-    'import/extensions': [
-      'error',
-      'never'
-    ],
-
-    // it seems we need this for asset loading
-    'import/no-dynamic-require': 'off',
-
-    // allow aliases
-    'import/no-unresolved': 'off',
-
-    // allow intentional console output
-    'no-console': [
-      'error',
-      {
-        allow: [
-          'error',
-          'warn'
-        ]
-      }
-    ],
-
     // allow in single-file components (according to HTML style guide)
-    'no-multiple-empty-lines': 'off',
-
-    // operators in multiline expressions at the beginning of the line
-    'operator-linebreak': [
+    'no-multiple-empty-lines': [
       'error',
-      'before'
+      { max: 5 }
     ],
 
     // use alphabetic sorting
